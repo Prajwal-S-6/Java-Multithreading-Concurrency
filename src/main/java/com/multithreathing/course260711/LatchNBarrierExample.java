9e7d1be2f3ba3e5f732d944dcea6bdc679d0b67a
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-07-13 23:26:10 +0530

    add RegularCollectionProblem demonstrating issues with concurrent modifications in a list

---
+++ src/main/java/com/multithreathing/course260711/LatchNBarrierExample.java
@@ -0,0 +1,100 @@
+package com.multithreathing.course260711;
+
+import java.util.concurrent.*;
+
+class Thread1 implements Runnable {
+
+    private final CountDownLatch countDownLatch;
+    private final CyclicBarrier cyclicBarrier;
+
+    public Thread1(CountDownLatch countDownLatch, CyclicBarrier cyclicBarrier) {
+        this.countDownLatch = countDownLatch;
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(100);
+            System.out.println("Thread1 working ....");
+            countDownLatch.countDown();
+
+            cyclicBarrier.await();
+            System.out.println("barrier tripped thread1 working again");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Thread2 implements Runnable {
+
+    private final CountDownLatch countDownLatch;
+    private final CyclicBarrier cyclicBarrier;
+
+    public Thread2(CountDownLatch countDownLatch, CyclicBarrier cyclicBarrier) {
+        this.countDownLatch = countDownLatch;
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(100);
+            System.out.println("Thread2 working ....");
+            countDownLatch.countDown();
+
+            cyclicBarrier.await();
+            System.out.println("barrier tripped thread2 working again");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Thread3 implements Runnable {
+
+    private final CountDownLatch countDownLatch;
+    private final CyclicBarrier cyclicBarrier;
+
+
+    public Thread3(CountDownLatch countDownLatch, CyclicBarrier cyclicBarrier) {
+        this.countDownLatch = countDownLatch;
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            countDownLatch.await();
+            System.out.println("Thread3 working ....");
+
+            cyclicBarrier.await();
+            System.out.println("barrier tripped thread3 working again");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+public class LatchNBarrierExample {
+
+    static void main() {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.execute(new Thread1(countDownLatch, cyclicBarrier));
+        executorService.execute(new Thread2(countDownLatch, cyclicBarrier));
+        executorService.execute(new Thread3(countDownLatch, cyclicBarrier));
+
+        executorService.shutdown();
+
+    }
+}
