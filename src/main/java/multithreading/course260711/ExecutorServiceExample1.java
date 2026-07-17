870e9db063349bae9c11ef6a084db03d1d5133d0
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-07-17 15:35:38 +0530

    Add deadlock solution examples using ReentrantLock

---
+++ src/main/java/multithreading/course260711/ExecutorServiceExample1.java
@@ -0,0 +1,87 @@
+package multithreading.course260711;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+class Task1 {
+
+    public void task(int i) {
+        try {
+            Thread.sleep(1000);
+            System.out.println("Doing task " + i + " " + Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            System.out.println("Tasks interrupted "+ i);
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("test");
+        }
+    }
+}
+
+
+public class ExecutorServiceExample1 {
+    static void main() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+//        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(6);
+//        Task1 task1 = new Task1();
+//        for (int i = 0; i < 100; i++) {
+//            int finalI = i;
+
+//            executorService.submit(() -> {
+//                task1.task(finalI);
+//            });
+////            executorService.execute(() -> {
+////                task1.task(finalI);
+////            });
+////            executorService.schedule(() -> {
+////                task1.task(finalI);
+////            }, 6, TimeUnit.SECONDS);
+//        }
+
+
+           /// execute() - Runnable type; doesnt throw checked exception exception travels to worker thread
+           /// submit() - Runnable, Callable type, throws checked exception and is stored in Future object
+        Runnable runnable = () -> {
+            System.out.println("Runnable...");
+        };
+
+        Callable callable = () -> {
+            System.out.println("Callable...");
+            Thread.sleep(10000);
+            return 6;
+        };
+
+        executorService.submit(runnable);
+        executorService.execute(runnable);
+        Future<Integer> futureObj = executorService.submit(callable);
+
+        executorService.shutdown();
+        try {
+            if(!executorService.awaitTermination(1, TimeUnit.SECONDS)) {
+               executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Exception was thrown " + e.getMessage());
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+
+        try {
+            System.out.println("Value: " + futureObj.get());
+        } catch (Exception  ee) {
+            System.out.println("Couldnt get the value within time" + ee);
+        }
+
+        List<Integer> list1 = Arrays.asList(1,2,3);
+        List<Integer> list2 = Arrays.asList(4,5);
+
+        List<List<Integer>> res = list1.stream()
+                .map(n1 -> list2.stream().map(n2 -> Arrays.asList(n1, n2)))
+                .flatMap(x -> x)
+                .collect(Collectors.toList());
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/multithreading/course260711/forkjoin/ForkJoinExample.java
@@ -0,0 +1,69 @@
+package multithreading.course260711.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+
+class ListAdditionWorker extends RecursiveAction {
+    int[] arr;
+
+    public ListAdditionWorker(int[] arr) {
+        this.arr = arr;
+    }
+
+    @Override
+    protected void compute() {
+        if(arr.length <= 2) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+            }
+        }
+        else {
+            int mid = (arr.length) / 2;
+            System.out.println("Splitting the array..." + arr.length);
+            ListAdditionWorker listAdditionWorker1 = new ListAdditionWorker(splitarray(arr, 0, mid));
+            ListAdditionWorker listAdditionWorker2 = new ListAdditionWorker(splitarray(arr, mid, arr.length));
+
+            invokeAll(listAdditionWorker1, listAdditionWorker2);
+        }
+    }
+
+    private int[] splitarray(int[] a, int low, int high) {
+        int[] res = new int[high - low];
+        for (int i=0; i < (high - low); i++) {
+            res[i] = a[low + i];
+        }
+        return res;
+    }
+}
+
+class Fibonacci extends RecursiveTask<Integer> {
+
+    int num;
+
+    public Fibonacci(int nthFibNum) {
+        this.num = nthFibNum;
+    }
+
+    @Override
+    protected Integer compute() {
+        if(num <= 1)
+        {
+            return num;
+        } else {
+            Fibonacci fibonacci = new Fibonacci(num - 1);
+            Fibonacci fibonacci1 = new Fibonacci(num - 2);
+            fibonacci1.fork();
+
+            return fibonacci.compute() + fibonacci1.join();
+        }
+    }
+}
+public class ForkJoinExample {
+
+    static void main() {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+//        forkJoinPool.invoke(new ListAdditionWorker(new int[] {1,2,3,4,5,6,7,8,9,10}));
+        System.out.println(forkJoinPool.invoke(new Fibonacci(8)));
+    }
+}
