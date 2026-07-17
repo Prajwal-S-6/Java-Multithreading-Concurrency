5cd1492717c1a86e8215be2b0c162e2673a3a9eb
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-07-17 15:35:40 +0530

    Add deadlock solution examples using ReentrantLock

---
+++ src/main/java/multithreading/course260711/LockRELock.java
@@ -0,0 +1,56 @@
+package multithreading.course260711;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Work {
+    public Lock lock = new ReentrantLock();
+
+    public void task1() {
+        lock.lock();
+        try {
+            System.out.println("Got lock1, performing task1...");
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+    public void task2() {
+        while (true)
+        {
+                if(lock.tryLock()){
+                    try {
+                        System.out.println("Got lock1, performing task2...");
+                        break;
+                    }
+                    finally {
+                        lock.unlock();
+                    }
+                } else {
+                    System.out.println("Doing other work meanwhile...");
+                }
+        }
+
+    }
+}
+public class LockRELock {
+
+    static void main() throws InterruptedException {
+        Work work = new Work();
+        Runnable r1 = work::task1;
+        Runnable r2 = work::task2;
+
+        var t1 = new Thread(r1);
+        var t2 = new Thread(r2);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+    }
+}
