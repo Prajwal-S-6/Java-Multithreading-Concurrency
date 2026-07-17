86c0c22a1cd41b2c21b8fe5abd138b67f16ad6e7
Prajwal-S-6 <prajwals12021999@gmail.com>
2026-07-17 15:35:29 +0530

    Add atomic and condition-based examples for multithreading

---
+++ src/main/java/multithreading/course260711/Deadlock.java
@@ -0,0 +1,61 @@
+package multithreading.course260711;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class DeadlockWork {
+    private final Lock lock1 = new ReentrantLock();
+    private final Lock lock2 = new ReentrantLock();
+
+    public void task1() {
+        try {
+            lock1.lock();
+            System.out.println("Doing task1");
+            Thread.sleep(1000);
+            lock2.lock();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock1.unlock();
+            lock2.unlock();
+        }
+    }
+    public void task2() {
+        try {
+            lock2.lock();
+            lock1.lock();
+            System.out.println("Doing task2");
+        } finally {
+            lock2.unlock();
+            lock1.unlock();
+        }
+    }
+
+}
+public class Deadlock {
+
+    static void main() throws InterruptedException {
+        DeadlockWork deadlockWork = new DeadlockWork();
+        Runnable r1 = () -> {
+            for (int i = 0; i < 10; i++) {
+                deadlockWork.task1();
+            }
+        };
+        Runnable r2 = () -> {
+            for (int i = 0; i < 10; i++) {
+                deadlockWork.task2();
+            }
+        };
+
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+
+
+}
